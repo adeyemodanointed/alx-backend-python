05f5dc7445f5a662f08f964f62d219bcce84c0c1
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-03-30 19:25:05 +0000

    correction to task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Type annotated add function"""
 
-def add(a: float, b: float) -> float:
-    return a + b
 
+def add(a: float, b: float) -> float:
+    """Function adds two numbers"""
+    return a + b
