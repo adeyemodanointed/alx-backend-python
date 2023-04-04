dc39a5ce808697e4192d2c8327afdd6a0fc11c46
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-04 13:59:37 +0000

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""The basics of async"""
+from random import uniform
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Function generates random float wait time and returns it"""
+    sleep_time = uniform(0, max_delay + 1)
+    await asyncio.sleep(sleep_time)
+    return sleep_time
