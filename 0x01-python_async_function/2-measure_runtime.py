294ec3c323455f5532e467e50b5388c2b76c6b25
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-04 21:31:20 +0000

    task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,8 +5,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
+    """measure an approximate elapsed time."""
     start = time.perf_counter()
-    await wait_n
+    asyncio.run(wait_n(n, max_delay))
     elapsed = time.perf_counter() - start
-    return(elapsed)
+    return (elapsed)
