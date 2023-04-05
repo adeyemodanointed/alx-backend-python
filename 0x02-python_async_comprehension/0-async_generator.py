63b774bf3e7b4dc5211c61bc58283034fba1a8d7
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-05 10:10:15 +0000

    function annotation task 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,8 @@ import asyncio
 
 
 async def async_generator():
-    """coroutine will loop 10 times, each time asynchronously wait
+    """
+    coroutine will loop 10 times, each time asynchronously wait
     1 second, then yield a random number between 0 and 10
     """
     for i in range(10):
