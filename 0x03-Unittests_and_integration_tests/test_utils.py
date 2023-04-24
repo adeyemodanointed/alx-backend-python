c312b986f931817a462d4fb9a0b065eca25b755a
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-24 21:39:17 +0000

    remove excess

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,13 +10,12 @@ class TestAccessNestedMap(unittest.TestCase):
     """Class to Test Nested Map function"""
 
     @parameterized.expand([
-        ("single", {"a": 1}, ("a",), 1),
-        ("single_dict", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("final", {"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
     def test_access_nested_map(
             self,
-            name: str,
             nested_map: Dict,
             path: Tuple[str],
             expected: Union[Dict, int]) -> None:
